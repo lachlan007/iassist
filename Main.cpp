c58c87c076369d3de189baa1bb917328869f1d05
kellermatth <kellermatth@gmail.com>
2012-06-20 16:25:34 +0000

    Removed no longer needed GPSd check.

--- Main.cpp
+++ Main.cpp
@@ -9,28 +9,9 @@
 int main(int argc, char *argv[])
 {
 
-	/*ButtonIO Button1;
-
-	Button1.openPort();
-	Button1.isButtonConnected();
-
-	printf(" %x\n", Button1.getRegisterContent(0x211));
-	printf(" %x\n", Button1.getRegisterContent(0x213));
-	printf(" %x\n", Button1.getRegisterContent(0x215));
-	printf(" %x\n", Button1.getRegisterContent(0x226));
-
-	Button1.closePort();*/
-
-
     QApplication a(argc, argv);
 
     IAssist w;
-
-    // Check if all the needed installations
-    if(SystemCommand::getStringFromSystemCommand("which gpsd")=="")
-    {
-    	UserDialog::warning("iAssist will start without GPS support. GPSD is not installed.");
-    }
 
     // Check if all the used directories are
     // existing, otherwise create them
