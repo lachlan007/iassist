2f6cea0153782f413c744baa18c96f5e407d39fb
kellermatth@gmail.com <kellermatth@gmail.com@fc90be2c-9618-a940-f4c0-015aae48617a>
2012-06-25 11:20:48 +0000

    Updated export script

--- trunk/csvExport.py
+++ trunk/csvExport.py
@@ -11,19 +11,26 @@ csvOutput = open(fileName, 'w')
 
 csvOutput.write('# TIME,TEMP,BUTTONNR,SESSIONNR,RESOLUTION'+'\n')
 
-sqlStmt = 'SELECT MeasurementProfileID, ButtonNr, SessionNr, SamplingStartTime, SamplingRate, TimeShift, Resolution FROM MeasurementProfiles'
+sqlStmt = 'SELECT MeasurementProfileID, ButtonNr, SessionNr, SamplingStartTime, SamplingRate, TimeShift, Resolution, CollectingTime FROM MeasurementProfiles'
+c2 = conn.cursor()
 for row in c.execute(sqlStmt):
+	# Not yet collected
+	if row[7] == None:
+		continue
 	t = (row[0],)
-	c.execute('SELECT MAX(MeasurementNr) FROM Measurement WHERE MeasurementProfileID=?', t)
+	c2.execute('SELECT MAX(MeasurementNr) FROM Measurement WHERE MeasurementProfileID=?', t)
 	# Clock drift compensation
-	numMeas = c.fetchone()[0]
+	numMeas = c2.fetchone()[0]
+	if numMeas == None:
+		print 'No data for button ' + row[1] + '?'
+		continue;
 	sampleStart = datetime.strptime(str(row[3]), '%d.%m.%Y %H:%M:%S')
 	samplingRate = int(row[4])
 	samplingDurationNoDrift = samplingRate * (numMeas-1)
 	timeShift = int(row[5])
 	samplingDurationWithDrift = samplingDurationNoDrift+timeShift
 	samplingRateReal = float(samplingDurationWithDrift) / (numMeas-1)
-	for row2 in c.execute('SELECT MeasurementNr,Measurement FROM Measurement WHERE MeasurementProfileID=?', t):
+	for row2 in c2.execute('SELECT MeasurementNr,Measurement FROM Measurement WHERE MeasurementProfileID=?', t):
 		out = []
 		ts = sampleStart+timedelta(0, (int(row2[0])-1)*samplingRateReal)
 		# Timestamp
